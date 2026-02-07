28ecd4e2cadd9b991e18aeb7fdc934da47e31a51
Mohammad Ab <wma22330@gmail.com>
2026-02-07 21:29:57 +0400

    feat: add nav_core library with Pose2D implementation

---
+++ cpp/nav_core/include/nav/pose2d.hpp
@@ -0,0 +1,8 @@
+struct Pose2D { 
+    double x, y, yaw_rad; 
+};
+
+double deg2rad(double deg);
+double rad2deg(double rad);
+double normalize_angle_rad(double a);
+Pose2D integrate_body_twist(const Pose2D& p, double v, double w_rad, double dt);
---
+++ cpp/nav_core/src/pose2d.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<nav/pose2d.hpp>
+#include <cmath>
+using namespace std;
+
+
+double deg2rad(double deg);
+double rad2deg(double rad);
+double normalize_angle_rad(double a);
+Pose2D integrate_body_twist(const Pose2D& p, double v, double w_rad, double dt);
+
+
+
+
+double deg2rad(double deg){
+    return deg * M_PI / 180;
+}
+
+
+double rad2deg(double rad){
+    return rad * 180 / M_PI;
+}
+
+
+double normalize_angle_rad(double yaw_raw){
+    while (yaw_raw > M_PI_2)
+        yaw_raw -= 2 * M_PI;
+    while (yaw_raw <= -M_PI)
+        yaw_raw += 2 * M_PI;
+    return yaw_raw;
+}
+
+
+Pose2D integrate_body_twist(const Pose2D& p, double v, double w_rad, double dt){
+    
+
+    double yaw2_rad = p.yaw_rad + w_rad * dt;
+    double x2 = p.x + v * dt * cos(p.yaw_rad);
+    double y2 = p.y + v * dt * sin(p.yaw_rad);
+    yaw2_rad = normalize_angle_rad(yaw2_rad);
+    return Pose2D {x2, y2, yaw2_rad};
+}
